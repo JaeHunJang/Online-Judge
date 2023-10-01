f1ae379b5d99e6728e1a2e8523ef3c7a814c05aa
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-01 13:48:34 +0900

    [unrated] Title: 배열 조각하기, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/README.md
+++ 프로그래머스/unrated/181893. 배열 조각하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열 조각하기 - 181893 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893?language=java) 
 
 ### 성능 요약
 
-메모리: 82.9 MB, 시간: 0.08 ms
+메모리: 82 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,13 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        
+        int start = 0;
+        int end = arr.length;
         for(int i = 0; i < query.length; i++){
-            if(i % 2 == 0) 
-                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
-            else
-                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            // if(i % 2 == 0) 
+            //     arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
+            // else
+            //     arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            if (i % 2 == 0) {
+                end = start + query[i] - 1;
+            } else {
+                start += query[i];
+            }
         }
-        return arr;
+
+        return Arrays.copyOfRange(arr, start, end + 2);
     }
 }
