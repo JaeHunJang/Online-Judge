1651babc7e311926e2f760f2c5b3722a77532d3a
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 02:35:04 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.11 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,34 +1,20 @@
 import java.util.*;
-import java.util.Collections;
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
+        int maxCount = 0;
+        int count = 0;
 
         HashMap<Integer,Integer> map = new HashMap<>();
         
         for(int n : array){
-            if(!map.containsKey(n)){
-                map.put(n, 1);
-            } else {
-                map.put(n, map.get(n)+1);
-            }
-        }
-        
-        List<Integer> keyset = new ArrayList<>(map.keySet());
-        
-        keyset.sort(new Comparator<Integer>() {
-            public int compare(Integer o1, Integer o2) {
-                return map.get(o1).compareTo(map.get(o2));
-            }
-        });
-        
-        int prev = 0;
-        for(int key : keyset){        
-            if(map.get(key) > prev) {
-                answer = key;
-                prev = map.get(key);
-                map.remove(key);
-            } else {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+            count = map.get(n);
+            
+            if(count > maxCount){
+                maxCount = count;
+                answer = n;
+            } else if (count == maxCount){
                 answer = -1;
             }
         }
