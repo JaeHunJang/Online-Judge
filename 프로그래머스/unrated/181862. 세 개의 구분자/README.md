fbac73f8a3782c46e70f7f15be0d2fe9de35fd5d
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 17:45:55 +0900

    [unrated] Title: 세 개의 구분자, Time: 38.77 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/README.md
+++ 프로그래머스/unrated/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [unrated] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862?language=java) 
 
 ### 성능 요약
 
-메모리: 115 MB, 시간: 32.21 ms
+메모리: 117 MB, 시간: 38.77 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,12 +3,20 @@ class Solution {
     public String[] solution(String myStr) {
         ArrayList<String> list = new ArrayList<>();
         
+        // myStr = myStr.replaceAll("b", "a").replaceAll("c", "a");
+        
+        // for(String str_c : myStr.split("a"))
+        //     if(!str_c.equals(""))
+        //         list.add(str_c);
+        
         for(String str_a : myStr.split("a")){
             for(String str_b : str_a.split("b"))
                 for(String str_c : str_b.split("c"))
                     if(!str_c.equals(""))
                         list.add(str_c);
         }
+        
+
         
         return list.size() > 0 ? list.toArray(new String[list.size()]) : new String[]{"EMPTY"};
     }
