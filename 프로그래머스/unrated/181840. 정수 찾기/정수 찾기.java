b3020f49c658271c5e9aa98e27bf327c16fb5e5e
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 13:27:55 +0900

    [unrated] Title: 정수 찾기, Time: 0.41 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] num_list, int n) {
+        Arrays.sort(num_list);
+        return Arrays.binarySearch(num_list, n) > 0 ? 1 : 0;
+    }
+}
