2ca594d1c0f367e523ff8c32d53ca7ace594bad2
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 23:26:06 +0900

    [level 0] Title: 숫자 찾기, Time: 9.54 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        
+        for(String s : (num+"").split("")){
+            answer++;
+            if((k+"").equals(s)) return answer;
+        }
+        
+        return -1;
+    }
+}
