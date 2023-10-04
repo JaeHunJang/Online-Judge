a0825aeed5df8a142a4952c70242a46db5be7ce7
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 17:21:56 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 9.46 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String binomial) {
+        int answer = 0;
+        String temp = "";
+        
+        for(char ch : binomial.toCharArray()){
+            if(ch == '+') { answer = 1; temp += ","; continue; }
+            if(ch == '-') { answer = 2; temp += ","; continue; }
+            if(ch == '*') { answer = 3; temp += ","; continue; }
+            if(Character.isDigit(ch)) temp += ch;
+        }
+        
+        int a = Integer.parseInt(temp.split(",")[0]);
+        int b = Integer.parseInt(temp.split(",")[1]);
+        
+        switch(answer){
+            case 1 : answer = a + b; break;
+            case 2 : answer = a - b; break;
+            case 3 : answer = a * b; break;
+        }
+        
+        return answer;
+    }
+}
