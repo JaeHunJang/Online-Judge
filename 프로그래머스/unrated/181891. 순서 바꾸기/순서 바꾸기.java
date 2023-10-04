bd8ddfb5688566bb3885c7630de2e3f269a89f5a
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 16:20:20 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        String temp1 = "";
-        String temp2 = "";
+        int[] answer = new int[num_list.length];
         
-        int i = 0;
-        for(int j : num_list){
-            if(i++ < n) temp1 += j;
-            else temp2 += j;
-        }
+//         String temp1 = "";
+//         String temp2 = "";
         
-        i = 0;
-        temp1 = temp2 + temp1;
-        int[] answer = new int[temp1.length()];
-        for(String s : temp1.split("")){
-            answer[i++] = Integer.parseInt(s);
+//         int i = 0;
+//         for(int j : num_list){
+//             if(i++ < n) temp1 += j;
+//             else temp2 += j;
+//         }
+        
+//         i = 0;
+//         temp1 = temp2 + temp1;
+//         for(String s : temp1.split("")){
+//             answer[i++] = Integer.parseInt(s);
+//         }
+        
+        for(int i = 0; i < answer.length; i++){
+            answer[(i + (answer.length - n)) % answer.length] = num_list[i];
         }
         
         return answer;
