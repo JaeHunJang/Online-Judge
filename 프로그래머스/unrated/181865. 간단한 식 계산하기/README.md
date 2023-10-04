3c22e3782e1b8067f5aac53f0af96023dd0a94de
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 17:25:38 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.14 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/README.md
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 간단한 식 계산하기 - 181865 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181865) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181865?language=java) 
 
 ### 성능 요약
 
-메모리: 78 MB, 시간: 9.46 ms
+메모리: 77.7 MB, 시간: 0.14 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,22 +1,31 @@
 class Solution {
     public int solution(String binomial) {
         int answer = 0;
-        String temp = "";
+//         String temp = "";
         
-        for(char ch : binomial.toCharArray()){
-            if(ch == '+') { answer = 1; temp += ","; continue; }
-            if(ch == '-') { answer = 2; temp += ","; continue; }
-            if(ch == '*') { answer = 3; temp += ","; continue; }
-            if(Character.isDigit(ch)) temp += ch;
-        }
+//         for(char ch : binomial.toCharArray()){
+//             if(ch == '+') { answer = 1; temp += ","; continue; }
+//             if(ch == '-') { answer = 2; temp += ","; continue; }
+//             if(ch == '*') { answer = 3; temp += ","; continue; }
+//             if(Character.isDigit(ch)) temp += ch;
+//         }
         
-        int a = Integer.parseInt(temp.split(",")[0]);
-        int b = Integer.parseInt(temp.split(",")[1]);
+//         int a = Integer.parseInt(temp.split(",")[0]);
+//         int b = Integer.parseInt(temp.split(",")[1]);
         
-        switch(answer){
-            case 1 : answer = a + b; break;
-            case 2 : answer = a - b; break;
-            case 3 : answer = a * b; break;
+//         switch(answer){
+//             case 1 : answer = a + b; break;
+//             case 2 : answer = a - b; break;
+//             case 3 : answer = a * b; break;
+//         }
+        
+        int a = Integer.parseInt(binomial.split(" ")[0]);
+        int b = Integer.parseInt(binomial.split(" ")[2]);
+        
+        switch(binomial.split(" ")[1]){
+            case "+" : answer = a + b; break;
+            case "-" : answer = a - b; break;
+            case "*" : answer = a * b; break;
         }
         
         return answer;
