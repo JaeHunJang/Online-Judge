9623fc57490284578a371034e434f5de69bc102a
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-04 15:16:23 +0900

    [unrated] Title: 0 떼기, Time: 6.35 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,15 +1,15 @@
 class Solution {
     public String solution(String n_str) {
-        String answer = "";
+//         String answer = "";
         
-        boolean flag = n_str.startsWith("0");
-        for(int i = 0; i < n_str.length(); i++){
-            if(!n_str.substring(i, n_str.length()).startsWith("0")) {
-                answer = n_str.substring(i, n_str.length());
-                break;
-            }
-        }
+//         for(int i = 0; i < n_str.length(); i++){
+//             if(!n_str.substring(i, n_str.length()).startsWith("0")) {
+//                 answer = n_str.substring(i, n_str.length());
+//                 break;
+//             }
+//         }
             
-        return answer;
+//         return answer;
+        return ""+Integer.parseInt(n_str);
     }
 }
