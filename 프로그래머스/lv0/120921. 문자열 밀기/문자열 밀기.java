9154300014790edfa916113a06fe4091ca94b004
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-05 21:32:29 +0900

    [level 0] Title: 문자열 밀기, Time: 1.66 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,19 @@
 class Solution {
     public int solution(String A, String B) {
-        if(A.equals(B)) return 0;
+//         if(A.equals(B)) return 0;
         
-        int answer = 0;
-        StringBuilder sb = new StringBuilder(A);
+//         int answer = 0;
+//         StringBuilder sb = new StringBuilder(A);
         
-        for(int i = 0; i < A.length(); i++){
-            System.out.println(sb.toString());
-            if(B.equals(sb.insert(0, sb.toString().charAt(A.length()-1)).deleteCharAt(sb.length()-1).toString())){
-                answer = i + 1;
-                break;
-            } else answer = -1;
-        }
-        return answer;
+//         for(int i = 0; i < A.length(); i++){
+//             System.out.println(sb.toString());
+//             if(B.equals(sb.insert(0, sb.toString().charAt(A.length()-1)).deleteCharAt(sb.length()-1).toString())){
+//                 answer = i + 1;
+//                 break;
+//             } else answer = -1;
+//         }
+//         return answer;
+        
+        return (B+B).indexOf(A);
     }
 }
