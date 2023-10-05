d2b94d53fa1fb31f98f05bf8a12afb3ec3d71ef3
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-05 23:25:22 +0900

    [level 0] Title: 옹알이 (1), Time: 1.43 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,25 +2,42 @@ import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+// 원본        
+//        String[] temp = {"aya", "ye", "woo", "ma"};
+
+//         ArrayList<String> list = new ArrayList();
+//         for(String s1 : temp){
+//             list.add(s1);
+//             for(String s2 : temp){
+//                 list.add(s1+s2);
+//                 for(String s3 : temp){
+//                     list.add(s1+s2+s3);
+//                     for(String s4 : temp)
+//                         list.add(s1+s2+s3+s4);
+//                 }
+//             }
+//         }
         
-        String[] temp = {"aya", "ye", "woo", "ma"};
+//         for(String b : babbling){
+//             for(String s : list){
+//                 if(b.equals(s)) answer++;
+//             }
+//         }
         
-        ArrayList<String> list = new ArrayList();
-        for(String s1 : temp){
-            list.add(s1);
-            for(String s2 : temp){
-                list.add(s1+s2);
-                for(String s3 : temp){
-                    list.add(s1+s2+s3);
-                    for(String s4 : temp)
-                        list.add(s1+s2+s3+s4);
-                }
-            }
-        }
+        // for(int i =0; i < babbling.length; i++) {
+        //     babbling[i] = babbling[i].replace("aya", "1");
+        //     babbling[i] = babbling[i].replace("woo", "1");
+        //     babbling[i] = babbling[i].replace("ye", "1");
+        //     babbling[i] = babbling[i].replace("ma", "1");
+        //     babbling[i] = babbling[i].replace("1", "");
+        //     if(babbling[i].isEmpty()) {
+        //         answer = answer + 1;
+        //     }
+        // }
         
-        for(String b : babbling){
-            for(String s : list){
-                if(b.equals(s)) answer++;
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
+                answer++;
             }
         }
         
