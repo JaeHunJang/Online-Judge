449e63862ffa9764dcc2b15c4873bf0f2cdbd5cb
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-05 17:26:15 +0900

    [level 0] Title: 소인수분해, Time: 99.22 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.math.*;
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        
+        ArrayList<Integer> list1 = new ArrayList();
+        ArrayList<Integer> list2 = new ArrayList();
+        BigInteger bigInt = BigInteger.ONE;
+        
+        while(bigInt.intValue() <= n){
+            bigInt = bigInt.nextProbablePrime();
+            list1.add(bigInt.intValue());
+        }
+        
+        while(n > 1){
+            for(int i : list1){
+                if(n % i == 0) {
+                    if(list2.indexOf(i) < 0)
+                        list2.add(i);
+                    n /= i;
+                    break;
+                }
+            }
+        }
+        
+        answer = new int[list2.size()];
+        int j = 0;
+        for(int i : list2){
+            answer[j++] = i;
+        }
+        
+        return answer;
+    }
+}
