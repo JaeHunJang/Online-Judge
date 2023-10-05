7c184e1aa9e18fbfd70c95d97a1afd6d19614ca0
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-05 18:13:53 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -13,6 +13,21 @@ class Solution {
             answer++;
         }
         
+        //answer += min * 2 - 1; //한줄로 완성
+
+        // 반복문 1번 사용
+        // for(int i=1; i<sides[0]+sides[1]; i++){
+        //     if(i>max){
+        //         if(min+max>i){
+        //             answer++;
+        //         }
+        //     } else {
+        //         if(min+i>max){
+        //             answer++;
+        //         }
+        //     }
+        // }
+        
         return answer;
     }
 }
