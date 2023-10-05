d1b2cf1146041881436fd8ffb72b746ce8448c7d
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-05 17:29:31 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/README.md
+++ 프로그래머스/lv0/120852. 소인수분해/README.md
@@ -1,10 +1,10 @@
 # [level 0] 소인수분해 - 120852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852?language=java) 
 
 ### 성능 요약
 
-메모리: 119 MB, 시간: 99.22 ms
+메모리: 76.5 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -4,31 +4,46 @@ class Solution {
     public int[] solution(int n) {
         int[] answer = {};
         
-        ArrayList<Integer> list1 = new ArrayList();
-        ArrayList<Integer> list2 = new ArrayList();
-        BigInteger bigInt = BigInteger.ONE;
-        
-        while(bigInt.intValue() <= n){
-            bigInt = bigInt.nextProbablePrime();
-            list1.add(bigInt.intValue());
+        ArrayList<Integer> list = new ArrayList();
+        for(int i = 2; n > 1;){
+            if(n % i == 0) {
+                if(list.indexOf(i) < 0)
+                    list.add(i);
+                n /= i;
+            } else i++;
         }
         
-        while(n > 1){
-            for(int i : list1){
-                if(n % i == 0) {
-                    if(list2.indexOf(i) < 0)
-                        list2.add(i);
-                    n /= i;
-                    break;
-                }
-            }
-        }
-        
-        answer = new int[list2.size()];
+        answer = new int[list.size()];
         int j = 0;
-        for(int i : list2){
+        for(int i : list){
             answer[j++] = i;
         }
+        
+//         ArrayList<Integer> list1 = new ArrayList();
+//         ArrayList<Integer> list2 = new ArrayList();
+//         BigInteger bigInt = BigInteger.ONE;
+        
+//         while(bigInt.intValue() <= n){
+//             bigInt = bigInt.nextProbablePrime();
+//             list1.add(bigInt.intValue());
+//         }
+        
+//         while(n > 1){
+//             for(int i : list1){
+//                 if(n % i == 0) {
+//                     if(list2.indexOf(i) < 0)
+//                         list2.add(i);
+//                     n /= i;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         answer = new int[list2.size()];
+//         int j = 0;
+//         for(int i : list2){
+//             answer[j++] = i;
+//         }
         
         return answer;
     }
