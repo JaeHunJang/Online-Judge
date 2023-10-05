1f750c7b44e83ec74ad52df3c8058e762fd0b45c
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-05 20:55:52 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -2,16 +2,31 @@ import java.math.*;
 
 class Solution {
     public int solution(int a, int b) {
-        BigInteger bi = new BigInteger(b+"");
-        int gcd = bi.gcd(new BigInteger(a+"")).intValue();
+//         BigInteger bi = new BigInteger(b+"");
+//         int gcd = bi.gcd(new BigInteger(a+"")).intValue();
         
-        b /= gcd;
+//         b /= gcd;
         
-        for(int i = 2; i < b; i++){
-            bi = new BigInteger(i + "");
-            if(bi.isProbablePrime(50) && b % i == 0 && i != 2 && i != 5) return 2;
+//         for(int i = 2; i < b; i++){
+//             bi = new BigInteger(i + "");
+//             if(bi.isProbablePrime(50) && b % i == 0 && i != 2 && i != 5) return 2;
+//         }
+        
+//         return b == 1 || a == b || (b % 2 == 0 || b % 5 == 0) ? 1 : 2;
+        
+        int answer = 0;
+        while(b % 2 == 0) {
+            b = b/2;
         }
-        
-        return b == 1 || a == b || (b % 2 == 0 || b % 5 == 0) ? 1 : 2;
+        while(b % 5 == 0) {
+            b = b/5;
+        }
+
+        if ((a / (double)b ) % 1 == 0) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        return answer;
     }
 }
