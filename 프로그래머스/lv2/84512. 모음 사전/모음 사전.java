15c5f7ef865345107e0fe804dee4181e2b08f261
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-06 16:19:19 +0900

    [level 2] Title: 모음 사전, Time: 34.35 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(String word) {
+        int answer = 0;
+        String[] dic = {"A", "E", "I", "O", "U"};
+        
+        ArrayList<String> list = new ArrayList();
+        for(int i = 0; i < 5; i++){
+            list.add(dic[i]);
+            for(int j = 0; j < 5; j++){
+                list.add(dic[i] + dic[j]);
+                for(int k = 0; k < 5; k++){
+                    list.add(dic[i] + dic[j] + dic[k]);
+                    for(int l = 0; l < 5; l++){
+                        list.add(dic[i] + dic[j] + dic[k] + dic[l]);
+                        for(int n = 0; n < 5; n++){
+                            list.add(dic[i] + dic[j] + dic[k] + dic[l] + dic[n]);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return list.indexOf(word)+1;
+    }
+}
