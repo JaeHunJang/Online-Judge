31ba920d4ffaabb42e32968a8c60066cb3442d49
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-06 23:58:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.67 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,16 +4,16 @@ class Solution {
         
         Map<String, Integer> map = new HashMap<>();
         
-        for(String name : participant){
+        for(String name : participant){ //명단 제작
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
-        for(String name : completion){
+        for(String name : completion){ //완주자 조사
             map.put(name, map.get(name) - 1);
             if(map.get(name) == 0) map.remove(name);
         }
         
-        return map.keySet().iterator().next();
+        return map.keySet().iterator().next(); //마지막 남은 사람 반환
         
     }
 }
