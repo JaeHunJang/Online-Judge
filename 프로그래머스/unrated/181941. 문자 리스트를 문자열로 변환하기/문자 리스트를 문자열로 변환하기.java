e231891614ea66c934bf2229c4c25cc454f37a8f
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-09 15:02:34 +0900

    [unrated] Title: 문자 리스트를 문자열로 변환하기, Time: 0.21 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        
+        //for(String s : arr)
+        //    answer += s;
+        answer = String.join("",arr);
+        
+        return answer;
+    }
+}
