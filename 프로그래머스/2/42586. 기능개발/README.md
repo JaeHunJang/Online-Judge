7825555e0c0229c11648d3473833ac7203e30814
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-14 23:00:28 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.14 ms
+메모리: 76.3 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 22:57:26
+2023년 10월 6일 23:0:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,53 +2,33 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> releases = new LinkedList();
-        
         Queue<int[]> progressQueue = new LinkedList<>();
 
+        //Queue 초기값 
         for(int i = 0; i < speeds.length; i++){
             progressQueue.add(new int[]{progresses[i], speeds[i]});
         }
-//         for(int i = 0;!progressQueue.isEmpty(); i++){
-//                 int[] temp = progressQueue.poll();
-            
-//             System.out.println(temp[0]+"|" +temp[1]);
-
-//         }
         
         while(!progressQueue.isEmpty()){
             int cnt = 0;
-            // for(int i = 0;!progressQueue.isEmpty(); i++){
-//                 int[] temp = progressQueue.poll();
-            
-//             System.out.println(temp[0]+"|" +temp[1]);
-
-//         }
             for(int i = 0; i < progressQueue.size(); i++){
                 int[] temp = progressQueue.poll();
                 temp[0] += temp[1];
                 progressQueue.add(temp);
-                
             }
             
-//             for(int i = 0;!progressQueue.isEmpty(); i++){
-//                 int[] temp = progressQueue.poll();
-            
-//             System.out.println(temp[0]+"|" +temp[1]);
-
-//         }
             while(!progressQueue.isEmpty() && progressQueue.peek()[0] >= 100) {
                 progressQueue.poll();
                 cnt++;
-                
             }
             if(cnt > 0) releases.add(cnt);
         }
         
+        //정답배열 만들기
         int[] answer = new int[releases.size()];
         for(int i = 0; i < releases.size(); i++){
             answer[i] = releases.get(i);
         }
-        // System.out.println(sb.toString());
         
         return answer;
     }
