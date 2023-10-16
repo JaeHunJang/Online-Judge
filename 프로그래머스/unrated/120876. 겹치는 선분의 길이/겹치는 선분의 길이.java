b4f13a64f15cd3e057177c9d9a0544a5148fc889
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-16 12:16:54 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 1.59 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -4,7 +4,7 @@ class Solution {
         int answer = 0;
         int[] l = new int[200];
         
-        Arrays.sort(lines, Comparator.comparingInt(x1 -> x1[0]));
+        Arrays.sort(lines, Comparator.comparingInt(x1 -> x1[0])); //start가 작은 순으로 정렬
         
         for(int[] line : lines){
             for(int[] line2 : lines){
@@ -18,12 +18,31 @@ class Solution {
                 } 
             }
         }
-        
-        if(Arrays.equals(lines[0], lines[1])) return Math.abs(Math.max(lines[0][0],lines[0][1]) - Math.min(lines[0][0],lines[0][1]));
+        //배열 내 선분이 모두 같은 경우 비교해서 처리
+        if(Arrays.equals(lines[0], lines[1])) return Math.abs(Math.max(lines[0][0],lines[0][1]) - Math.min(lines[0][0],lines[0][1])); 
         
         for(int n : l){
             if(n == 1) answer++;
         }
         return answer;
+        
+        /*
+        int[] thickness = new int[200];
+        for (int[] line : lines) { //선분을 처음부터 전부 채워넣어서 값이 1이상이면 겹친선분
+            int start = line[0] + 100;
+            int end = line[1] + 100;
+            for (int i = start; i < end; ++i) {
+                ++thickness[i];
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < 200; ++i) {
+            if (thickness[i] > 1) {
+                ++answer;
+            }
+        }
+        return answer;
+        
+        */
     }
 }
