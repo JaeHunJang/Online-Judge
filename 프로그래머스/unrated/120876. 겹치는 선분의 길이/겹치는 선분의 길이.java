136e140b94086ec77835aa35c669d7114607a0d6
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-16 12:08:44 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 2.54 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        Queue<int[]> q = new LinkedList();
+        int[] l = new int[202];
+        
+        Arrays.sort(lines, Comparator.comparingInt(x1 -> x1[0]));
+        
+        for(int[] line : lines){
+            for(int[] line2 : lines){
+                if(Arrays.equals(line, line2)) continue;
+                if(line[1] > line2[0] || line[1] < line2[0]){
+                    int end = Math.min(line[1], line2[1]);
+                    int start = Math.max(line[0], line2[0]);
+                    for(int i = start; i < end; i++){
+                        // System.out.println(i+"");
+                        l[i+100] = 1;
+                    }
+                } 
+            }
+        }
+        
+        if(Arrays.equals(lines[0], lines[1])) return Math.abs(Math.max(lines[0][0],lines[0][1]) - Math.min(lines[0][0],lines[0][1]));
+        
+        for(int n : l){
+            if(n == 1) answer++;
+        }
+        return answer;
+    }
+}
