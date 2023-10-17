4c04b4ebe0f14de33980c6a63b0959df3e741ac6
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-17 20:59:46 +0900

    [level 2] Title: 주식가격, Time: 27.85 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        // Arrays.fill(answer, -1);
+//         LinkedList<Integer> q = new LinkedList();
+//         Stack<Integer> stack = new Stack();
+        
+//         for(int p : prices){
+//             q.add(p);
+//         }
+        
+//         for(int i = 0; i < prices.length; i++){
+//             // int price = q.pop();
+//             while(!stack.isEmpty() && stack.peek() > prices[i]){
+//                 System.out.println(prices[i]+"");
+//                 answer[i] = stack.size();
+//                 stack.pop();
+//             }
+//             if(answer[i] == -1) answer[i] = stack.size();
+            
+//             stack.push(prices[i]);
+//         }
+        
+//         int idx = 0;
+//         while(!q.isEmpty()){
+//             int price = q.pop();
+//             for(int i = 0; i < q.size(); i++){
+//                 answer[idx] = i+1;
+
+//                 if(price > q.get(i)) { 
+//                     // System.out.println(price + "|" + temp[i]);
+//                     break;
+//                 }
+//             }
+            
+//             // if(q.indexOf(price) > 0) answer[idx] = q.indexOf(price);
+//             // if(answer[idx] == -1) answer[idx] = q.size();
+//             idx++;
+//         }
+        
+        for(int j = 0; j < prices.length; j++){
+            for(int i = j+1; i < prices.length; i++){
+                answer[j]++;
+
+                if(prices[j] > prices[i]) { 
+                    // System.out.println(price + "|" + temp[i]);
+                    break;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
