260c1cfcf4a50871e6a518a32610da39e078eb74
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-23 18:02:36 +0900

    [level 2] Title: 괄호 회전하기, Time: 5.72 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String open = "({[";
+        String close = ")}]";
+        for(int i = 0; i < s.length(); i++){
+            String temp = s.substring(i, s.length()) + s.substring(0, i); 
+            Stack<String> stack = new Stack();
+            
+            for(String t : temp.split("")){
+                if(close.contains(t)) {
+                    int idx = close.indexOf(t);
+                    if(!stack.isEmpty() 
+                       && stack.peek().equals(open.charAt(idx) + "")) {
+                        stack.pop();
+                    } else stack.push(t);
+                } else stack.push(t);
+            }
+            if (stack.isEmpty()) {
+                answer++;
+                // System.out.println(temp);
+            }
+            
+        }
+        return answer;
+    }
+}
