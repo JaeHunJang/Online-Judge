e34f30ea6f43a50000236b549f19488d8d1208c8
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-10-24 11:29:58 +0900

    [level 1] Title: [1차] 다트 게임, Time: 14.64 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,37 +1,35 @@
+import java.util.*;
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
         String score = "SDT";
         String option = "*#";
         String num = "";
-        int[] scores = new int[3];
-        int idx = 0;
-        String[] darts = dartResult.split("");
-        for(int i = 0; i < darts.length; i++){
-            
-            if(!score.contains(darts[i]) && !option.contains(darts[i])){
-                num += darts[i];
+        Stack<Integer> stack = new Stack();
+        
+        for(int i = 0; i < dartResult.length(); i++){
+            char ch = dartResult.charAt(i);
+            if(Character.isDigit(ch)){
+                num += ch + "";
             } else {
                 if(!num.equals("")) {
-                    scores[idx] = Integer.parseInt(num);
+                    stack.push((int)Math.pow(Integer.parseInt(num), score.indexOf(ch+"") + 1));
                     num = "";
-                    scores[idx] = (int)Math.pow(scores[idx], score.indexOf(darts[i]) + 1);
                 }
-                if(i+1 < darts.length && option.contains(darts[i+1])) {
-                    if(option.indexOf(darts[++i]) == 0) {
-                        if(idx - 1 >= 0)
-                            scores[idx - 1] *= 2;
-                        scores[idx] *= 2;
-                    } else if (option.indexOf(darts[i]) == 1) {
-                        scores[idx] *= -1;
-                    }
+                if(option.indexOf(ch+"") == 0) {
+                    int temp = stack.pop() * 2;
+                    System.out.println(temp+"");
+                    if(!stack.isEmpty())
+                        stack.push(stack.pop() * 2);
+                    stack.push(temp);
+                } else if (option.indexOf(ch+"") == 1) {
+                    stack.push(stack.pop() * -1);
                 }
-                idx++;
             }
         }
         
-        for(int n : scores){
-            answer += n;
+        while(!stack.isEmpty()){
+            answer += stack.pop();
         }
         
         return answer;
