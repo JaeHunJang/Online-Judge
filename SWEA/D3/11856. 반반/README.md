b41ed87a9d61b1f4a55ce1e9937fa95c0c15451e
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-11-27 15:53:31 +0900

    [D3] Title: 반반, Time: 205 ms, Memory: 27,656 KB -BaekjoonHub

---
+++ SWEA/D3/11856. 반반/README.md
@@ -0,0 +1,15 @@
+# [D3] 반반 - 11856 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXjS1GXqZ8gDFATi) 
+
+### 성능 요약
+
+메모리: 27,656 KB, 시간: 205 ms, 코드길이: 478 Bytes
+
+### 제출 일자
+
+2023-11-27 15:53
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/11856. 반반/반반.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            String s = sc.next();
+            String answer = "No";
+            if(Arrays.stream(s.split("")).distinct().count() == 2) answer = "Yes";
+            
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+}
