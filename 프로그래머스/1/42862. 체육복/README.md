705629e1a9278c9b3d9b36860d883a9a75c0a454
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-12-08 14:31:56 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.55 ms
+메모리: 73.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 14:25:8
+2023년 12월 5일 14:31:55
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,31 +2,53 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] d = {-1, 1};
-        List<Integer> lostList = new ArrayList<>();
-        List<Integer> reserveList = new ArrayList<>();
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        for(int l : lost) {
-            lostList.add(l);
-        }
-        for(int r : reserve) {
-            if(lostList.contains(r)) {
-                lostList.remove(Integer.valueOf(r));
-            } else reserveList.add(r);
-        }
-        
-        for(int r : reserveList) {
-            for(int i = 0; i < d.length; i++) {
-                int idx = lostList.indexOf(r + d[i]);
-                if(idx != -1) {
-                    lostList.remove(idx);
-                    break;
-                }
+        int[] people = new int[n+2];
+        int answer = n;
+
+        for (int l : lost) 
+            people[l]--;
+        for (int r : reserve) 
+            people[r]++;
+
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                } else if(people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                } else 
+                    answer--;
             }
         }
+        return answer;
+// /* 내 풀이 비용이 너무 크다... */
+//         int[] d = {-1, 1};
+//         List<Integer> lostList = new ArrayList<>();
+//         List<Integer> reserveList = new ArrayList<>();
+//         Arrays.sort(lost);
+//         Arrays.sort(reserve);
         
-        return n - lostList.size();
+//         for(int l : lost) {
+//             lostList.add(l);
+//         }
+//         for(int r : reserve) {
+//             if(lostList.contains(r)) {
+//                 lostList.remove(Integer.valueOf(r));
+//             } else reserveList.add(r);
+//         }
+        
+//         for(int r : reserveList) {
+//             for(int i = 0; i < d.length; i++) {
+//                 int idx = lostList.indexOf(r + d[i]);
+//                 if(idx != -1) {
+//                     lostList.remove(idx);
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return n - lostList.size();
     }
 }
