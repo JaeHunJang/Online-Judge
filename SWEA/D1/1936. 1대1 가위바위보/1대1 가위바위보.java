1f87dcb36dabb19d097a06f317617bb82996dba5
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2023-12-29 16:14:18 +0900

    [D1] Title: 1대1 가위바위보, Time: 126 ms, Memory: 20,160 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
-
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-        
+ 
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+         
         if(a + b == 4) {
             if(a > b) System.out.print("B");
             else System.out.print("A");
@@ -16,7 +14,5 @@ class Solution
             if(a > b) System.out.print("A");
             else System.out.print("B");
         }
-            
-
-	}
+    }
 }
