8f48810fd23500869b12e4c5480f6ff6ffd367d6
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-01-08 19:39:43 +0900

    [Silver IV] Title: 수 찾기, Time: 1236 ms, Memory: 52048 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < m; i++) {
+			if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) > -1)
+				System.out.println("1");
+			else System.out.println("0");
+		}
+	}
+}
