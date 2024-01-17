b361d8145b815c49374a7f787199b5074331410c
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-01-17 21:46:48 +0900

    [Silver I] Title: IOIOI, Time: 276 ms, Memory: 22056 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,8 +6,7 @@ public class Main {
 	private static StringBuilder sb = new StringBuilder();
 	private static int N;
 	private static int M;
-	private static String S;
-	private static String P;
+	private static char[] S;
 	private static int result;
 	
 	public static void main(String[] args) throws Exception {
@@ -19,24 +18,24 @@ public class Main {
 	private static void init() throws Exception {
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-		S = br.readLine();
+		S = br.readLine().toCharArray();
 		result = 0;
-		generateP();
 	}
 	
 	private static void solve() throws Exception {
-		for (int i = 0; i < M; i++) {
-			if (S.substring(i).startsWith(P)) result++;
+		int count = 0;
+		for (int i = 1; i < M-1;) {
+			if ('I' == S[i-1] && 'O' == S[i] && 'I' == S[i+1]) { //IOI인지 확인
+				count++;
+				if (count == N) { // Pn 만큼 반복되었으면 개수증가, count감소(다음 개수 확인)
+					result++;
+					count--; 
+				}
+				i += 2; //OI 반복만큼 인덱스 이동
+			} else {
+				count = 0; // I가 아니면 초기화, 한칸 이동
+				i++;
+			}
 		}
-	}
-	
-	private static void generateP() {
-		for (int i = 0; i < N*2+1; i++) {
-			if (i % 2 == 0)
-				sb.append("I");
-			else sb.append("O");
-		}
-		P = sb.toString();
-		sb.setLength(0);
 	}
 }
