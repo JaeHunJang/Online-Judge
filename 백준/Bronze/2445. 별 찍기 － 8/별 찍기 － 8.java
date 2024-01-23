8b72d6df80a81373994bbbc7d00ceb23511559ca
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-01-23 22:27:52 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 140 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static StringTokenizer st;
+	private static int N;
+	public static void main(String[] args) throws Exception {
+		init();
+		solve();
+		System.out.println(sb.toString());
+	}
+
+	private static void init() throws Exception {
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+	}
+
+	private static void solve() throws Exception {
+		for (int i = 1; i <= N; i++) {
+			int j = 1;
+			for (; j <= i; j++) {
+				sb.append("*");
+			}
+			for (; j <= 2*N-i; j++) {
+				sb.append(" ");
+			}
+			for (; j <= 2*N; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		for (int i = 1; i <= N; i++) {
+			int j = 1;
+			for (; j <= N-i; j++) {
+				sb.append("*");
+			}
+			for (j=1; j <= 2*i; j++) {
+				sb.append(" ");
+			}
+			for (j=1; j <= N-i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+	}
+}
