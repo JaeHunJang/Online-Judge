4ddd0710fc3180f8f8a11ff37513b4721dc4b7d9
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-01-24 22:03:34 +0900

    [Silver IV] Title: 큐, Time: 192 ms, Memory: 20408 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static int N;
+	private static Queue<String> q;
+	private static String last;
+	public static void main(String[] args) throws Exception {
+		init();
+		solve();
+		System.out.println(sb.toString());
+	}
+
+	private static void init() throws Exception {
+		N = Integer.parseInt(br.readLine());
+		q = new LinkedList<>();
+		for (int i = 0; i < N-1; i++) {
+			solve();
+		}
+	}
+
+	private static void solve() throws Exception {
+		String cmd = br.readLine();
+		if (cmd.contains("push")) {
+			last = cmd.split(" ")[1];
+			q.add(last);
+		} else {
+			cmd = cmd.split(" ")[0];
+			if ("size".equals(cmd)) {
+				sb.append(q.size());
+			} else if ("empty".equals(cmd)) {
+				sb.append(q.isEmpty() ? "1" : "0");
+			} else if ("pop".equals(cmd)) {
+				sb.append(q.isEmpty() ? "-1" : q.poll());
+			} else {
+				if (q.isEmpty()) sb.append("-1");
+				else {
+					if ("front".equals(cmd)) {
+						sb.append(q.peek());
+					} else {
+						sb.append(last);
+					}
+				}
+			}
+			sb.append("\n");
+		}
+	}
+}
