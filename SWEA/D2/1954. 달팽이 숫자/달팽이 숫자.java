fd970fa3e3ff2cf18c94925837d596f5cdb0c5fa
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-01-31 21:02:45 +0900

    [D2] Title: 달팽이 숫자, Time: 106 ms, Memory: 18,452 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,46 +1,64 @@
-import java.util.*;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            System.out.println("#" + test_case);
-            int n = sc.nextInt();
-            int[][] answer = new int[n][n];
-            int i = 1;
-            int x = 0;
-            int y = 0;
-            answer[y][x] = i++;
-            while(i <= n*n) {
-                while(x < n-1 && answer[y][x+1] == 0)  {
-                    x++;
-                    answer[y][x] = i++;
-                }
-                while(y < n-1 && answer[y+1][x] == 0) {
-                    y++;
-                    answer[y][x] = i++;
-                }
-                while(x > 0 && answer[y][x-1] == 0) {
-                    x--;
-                    answer[y][x] = i++;
-                }
-                while(y > 0 && answer[y-1][x] == 0) {
-                    y--;
-                    answer[y][x] = i++;
-                }
-            }
-            for(int[] arr : answer) {
-                for(int a : arr) 
-                    System.out.print(a + " ");
-                System.out.println();
-            }
-            
+public class Solution {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static int T;
+	private static int N;
+	private static int[][] map;
+	private static int[][] deltas = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+	public static void main(String[] args) throws Exception {
+		init();
+		print();
+	}
+	private static void print() {
+		System.out.println(sb.toString());
+	}
+	
+	private static void init() throws Exception { 
+		T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + "\n");
+			
+			N = Integer.parseInt(br.readLine()); //배열 크기
+			map = new int[N][N];
+			solve();
+			printResult();
 		}
+	}
+	
+	private static void solve() {
+		int i = 0;
+		int j = 0;
+		int cnt = 1;
+		map[i][j] = cnt;
+		while (cnt < N*N) {
+			for (int d = 0; d < deltas.length; d++) {
+				int nr = i + deltas[d][0];
+				int nc = j + deltas[d][1];
+				
+				while (isIn(nr,nc) && map[nr][nc] == 0) {
+					i = nr;
+					j = nc;
+					map[nr][nc] = ++cnt;
+					nr += deltas[d][0];
+					nc += deltas[d][1];
+				}
+			}
+		}
+	}
+	
+	private static void printResult() {
+		for (int[] row : map) {
+			for (int i : row) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+		}
+	}
+	
+	private static boolean isIn(int i, int j) {
+		return i >= 0 && i < N && j >= 0 && j < N;
 	}
 }
