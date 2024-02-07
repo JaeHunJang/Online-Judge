97ebfc074db39042b1a6bd1ede1a996f26e993a3
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-02-07 21:36:14 +0900

    [Silver I] Title: 절댓값 힙, Time: 332 ms, Memory: 25452 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,42 +2,47 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static StringBuilder sb = new StringBuilder();
 	private static int N;
-	private static PriorityQueue<int[]> pq;
+	private static PriorityQueue<Integer> pq;
+	private static String command;
 	public static void main(String[] args) throws Exception {
 		init();
 		solve();
+		print();
+	}
+	private static void print() {
 		System.out.println(sb.toString());
 	}
-
-	private static void init() throws Exception {
-		N = Integer.parseInt(br.readLine());
-		
-		pq = new PriorityQueue<>(new Comparator<int[]>() {
+	
+	private static void init() throws Exception { 
+		pq = new PriorityQueue<>(new Comparator<Integer>() { // 절대값 힙
 			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (o1[0] == o2[0]) {
-					return o1[1] - o2[1];
+			public int compare(Integer o1, Integer o2) {
+				if (Math.abs(o1) == Math.abs(o2)) { // 절대값이 같은게 여러개면
+					return o1 - o2; // 실제 숫자가 작은 것으로 정렬
 				}
-				return o1[0] - o2[0];
+				return Math.abs(o1) - Math.abs(o2);
 			}
 		});
+		N = Integer.parseInt(br.readLine()); // 숫자
+		
+		command = "";
 	}
 	
-
 	private static void solve() throws Exception {
 		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if (num == 0) {
-				if (pq.isEmpty()) sb.append(0);
-				else sb.append(pq.poll()[1]);
-				sb.append("\n");
+			command = br.readLine();
+			if (!"0".equals(command)) {
+				pq.add(Integer.parseInt(command));
 			} else {
-				pq.add(new int[]{Math.abs(num), num});
+				if (pq.isEmpty()) sb.append("0");
+				else sb.append(pq.poll());
+				sb.append("\n");
 			}
 		}
 	}
