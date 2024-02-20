9d119280645f60ab04895e5041088f840c779c70
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-02-20 19:44:27 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 15940 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,80 +1,74 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static StringBuilder sb = new StringBuilder();
-	private static int N;
+	private static StringTokenizer st;
+	private static int N, count1, count2, deltas[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 	private static String[][] map;
 	private static boolean[][] visited;
-	private static boolean[][] visited2;
-	private static int[][] deltas = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }};
-	private static final String[] RGB = { "R", "G", "B" };
-	private static final String[] RGB2 = { "RG", "B" };
-    
-	private static int count;
 	public static void main(String[] args) throws Exception {
 		init();
-		
+		solve();
+		print();
+	}
+	private static void print() {
 		System.out.println(sb.toString());
 	}
-
-	private static void init() throws Exception {
-		N = Integer.parseInt(br.readLine());
+	
+	private static void init() throws Exception { 
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 구역의 크기
 		
 		map = new String[N][N];
 		visited = new boolean[N][N];
-		visited2 = new boolean[N][N];
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().split("");
+		}
 		
-		for (int i = 0; i < N; i++) {
-			int j = 0;
-			for (String color : br.readLine().split("")) {
-				map[i][j++] = color;
-			}
-		}
-		solve();
+		count1 = 0; // 적록색약이 아닌 구역 개수
+		count2 = 0; // 적록색약인 구역 개수
 	}
 	
-
-	private static void solve() {
+	private static void solve() throws Exception {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < RGB.length; k++) {
-					if (map[i][j].equals(RGB[k]) && !visited[i][j]) {					
-						count++;
-						visited[i][j] = true;
-						dfs(i, j, RGB[k], visited);
-					}
-				}
+				if (visited[i][j]) continue;
+				dfs(i, j, map[i][j]); // 적록색약이 아닌 사람
+				count1++;
 			}
 		}
-		sb.append(count).append(" ");
-		count = 0;
+		visited = new boolean[N][N]; // dfs를 2번 하기 때문에 visited 배열 다시 초기화
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < RGB2.length; k++) {
-					if (RGB2[k].contains(map[i][j]) && !visited2[i][j]) {					
-						count++;
-						visited2[i][j] = true;
-						dfs(i, j, RGB2[k], visited2);
-					}
-				}
+				if (visited[i][j]) continue;
+				if ("RG".contains(map[i][j])) // 적록색약인 사람
+					dfs(i, j, "RG");
+				else
+					dfs(i, j, map[i][j]);
+				count2++;
 			}
 		}
-		sb.append(count);
+		sb.append(count1).append(" ").append(count2);
 	}
 	
-	private static void dfs(int i, int j, String color, boolean[][] visited) {
+	private static void dfs(int r, int c, String color) {
+		visited[r][c] = true;
+		
 		for (int d = 0; d < deltas.length; d++) {
-			int ni = i + deltas[d][0];
-			int nj = j + deltas[d][1];
-			
-			if (ni >= 0 && ni < N && nj >= 0 && nj < N)	{
-				if (color.contains(map[ni][nj]) && !visited[ni][nj]) {
-					visited[ni][nj] = true;
-					dfs(ni, nj, color, visited);
-				}
+			int nr = r + deltas[d][0];
+			int nc = c + deltas[d][1];
+			if (!isIn(nr, nc)) continue;
+			if (visited[nr][nc]) continue;
+			if (color.contains(map[nr][nc])) {
+				dfs(nr, nc, color);
 			}
 		}
+	}
+	
+	private static boolean isIn(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
 	}
 }
