4b4764c321ddbc62f374a81a69b522af0827ea00
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-02-21 19:48:03 +0900

    [Gold V] Title: 암호 만들기, Time: 148 ms, Memory: 16016 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,53 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static StringTokenizer st;
 	private static StringBuilder sb = new StringBuilder();
-	private static StringBuilder sb2 = new StringBuilder();
-	private static int L;
-	private static int C;
-	private static String[] arr;
-	
+	private static StringTokenizer st;
+	private static int L, C;
+	private static char characters[], password[];
+	private static String vowel = "aeiou";
 	public static void main(String[] args) throws Exception {
 		init();
 		solve();
-		printResult();
+		print();
 	}
-	private static void printResult() {
+	private static void print() {
 		System.out.println(sb.toString());
 	}
 	
-	private static void init() throws Exception {
+	private static void init() throws Exception { 
 		st = new StringTokenizer(br.readLine());
-		L = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		arr = br.readLine().split(" ");
-		
-		Arrays.sort(arr);
-	}
-	private static void solve() {
-		combination(0, "");
+		L = Integer.parseInt(st.nextToken()); // 암호 길이
+		C = Integer.parseInt(st.nextToken()); // 암호 조합 문자
+
+		password = new char[L];
+		characters = br.readLine().replaceAll(" ", "").toCharArray();
+		Arrays.sort(characters);
 	}
 	
-	public static void combination(int start, String pw) {
-		if (pw.length() == L) {
-			sb2.append(pw);
-			String temp = pw.replaceAll("[aeiou]", ""); // 자음만 남김
-			// 자음이 2개 이상, 모음이 1개 이상인것만 저장
-			if (temp.length() >=2 && pw.length() - temp.length() >=1) {
-				sb.append(pw).append("\n");
-			}
+	private static void solve() throws Exception {
+		combi(0, 0, 0);
+	}
+	
+	private static void combi(int cnt, int start, int vowelCount) {
+		if (cnt == L) {
+			if (vowelCount >= 1 && L - vowelCount >= 2)
+				sb.append(password).append("\n");
 			return;
 		}
 		
 		for (int i = start; i < C; i++) {
-			combination(i+1, pw + arr[i]);
+			password[cnt] = characters[i];
+			if (vowel.contains(characters[i] + ""))
+				combi(cnt+1, i+1, vowelCount + 1);
+			else
+				combi(cnt+1, i+1, vowelCount);
 		}
 	}
 }
