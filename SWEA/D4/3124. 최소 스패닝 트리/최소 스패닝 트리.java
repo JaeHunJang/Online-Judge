e63df4bc8fefc4dec76dc1c0ca574736994fe59a
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-02-25 23:51:22 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,909 ms, Memory: 115,160 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,87 +2,97 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static int T, V, E, parents[];
-	static Edge list[];
-	static class Edge implements Comparable<Edge> {
-		int from,to,w;
-
-		public Edge(int from, int to, int w) {
-			this.from = from;
-			this.to = to;
-			this.w = w;
-		}
-		
-		public int compareTo(Edge o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
-	public static void main(String[] args) throws Exception {
-		init();
-		print();
-	}
-	private static void print() {
-		System.out.println(sb.toString());
-	}
-	
-	private static void init() throws Exception { 
-		T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
-			
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());  
-			E = Integer.parseInt(st.nextToken());
-			parents = new int[V+1];
-			for (int i = 1; i <= V; i++) {
-				parents[i] = i; // 자기 자신을 부모로
-			}
-			
-			list = new Edge[E];
-			int from, to, weight;
-			for (int i = 0; i < E; i++) {
-				st = new StringTokenizer(br.readLine());
-				from = Integer.parseInt(st.nextToken());
-				to = Integer.parseInt(st.nextToken());
-				weight = Integer.parseInt(st.nextToken());
-				
-				list[i] = new Edge(from, to, weight);
-			}
-			
-			Arrays.sort(list);
-		
-			solve();
-		}
-	}
-	
-	private static void solve() throws Exception {
-		long sum = 0, cnt = 0;
-		for (int i = 0; i < E; i++) {
-			if (union(list[i].from, list[i].to)) {
-				sum += list[i].w;
-				cnt++;
-			}
-			if (cnt == V-1) break;
-		}
-		sb.append(sum).append("\n");
-	}
-	
-	static int find(int x) {
-		if (parents[x] == x) return parents[x];
-		return parents[x] = find(parents[x]);
-	}
-	
-	static boolean union(int a, int b) {
-		int pa = find(a);
-		int pb = find(b);
-		if (pa == pb) return false;
-		parents[pa] = pb;
-		return true;
-	}
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+    private static int T, V, E, parents[];
+    private static Edge edgeList[];
+     
+    static class Edge implements Comparable<Edge> {
+        int from, to, weight;
+         
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+         
+        public int compareTo(Edge o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        init();
+        print();
+    }
+    private static void print() {
+        System.out.println(sb.toString());
+    }
+     
+    private static void init() throws Exception { 
+        T = Integer.parseInt(br.readLine());
+         
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("#").append(tc).append(" ");
+             
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+             
+            edgeList = new Edge[E]; // 간선리스트 생성
+            int from,to,weight;
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                from = Integer.parseInt(st.nextToken());
+                to = Integer.parseInt(st.nextToken());
+                weight = Integer.parseInt(st.nextToken());
+                edgeList[i] = new Edge(from, to, weight);
+            }
+             
+            solve();
+        }
+    }
+     
+    private static void solve() throws Exception {
+        // 전처리
+        Arrays.sort(edgeList); // 간선 가중치 기준으로 정렬
+ 
+        // 1. make-set
+        make();
+         
+        // 2. 최소 비용 순으로 연결
+        long totalWeight = 0; // 최소 비용
+        int cnt = 0; // 연결된 간선의 수
+        for (Edge edge : edgeList) {
+            if (!union(edge.from, edge.to)) continue; // 이미 집합(트리)에 속해있으면 싸이클 발생
+            totalWeight += edge.weight;
+            if (++cnt == V-1) break; // 간선의 수가 V-1이 되면 최소신장트리 완성
+        }
+         
+        sb.append(totalWeight).append("\n");
+    }
+     
+    private static void make() {
+        parents = new int[V+1];
+        for (int i = 1; i <= V; i++) {
+            parents[i] = i;
+        }
+    }
+     
+    private static int find(int a) {
+        if (parents[a] == a) return a;
+        return parents[a] = find(parents[a]); // path 압축
+    }
+     
+    private static boolean union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot == bRoot) return false; // 이미 같은 집합에 속함
+        parents[bRoot] = aRoot;
+ 
+        return true;
+         
+    }
 }
