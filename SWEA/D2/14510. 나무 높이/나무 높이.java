05c0a080e04c87f600014c7f6f217666e9dca06a
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-03-04 00:33:39 +0900

    [D2] Title: 나무 높이, Time: 117 ms, Memory: 21,220 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+/*https://ghs4593.tistory.com/12*/
 public class Solution {
 	static StringBuilder sb = new StringBuilder();
 	static int N, trees[];
