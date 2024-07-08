49bbbf48b5ab87e47c074f108f3632f4763cd938
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2024-07-08 23:44:27 +0900

    [Gold V] Title: 치킨 배달, Time: 184 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,94 +1,79 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// 치킨 배달 / 40분
 public class Main {
-	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static StringBuilder sb = new StringBuilder();
-	private static StringTokenizer st;
-	private static int N, M, minDist;
-	private static List<Coordinate> house, chicken;
-	private static Coordinate selectedChicken[];
-	public static void main(String[] args) throws Exception {
-		init();
-		solve();
-		print();
-	}
-	private static void print() {
-		System.out.println(sb.toString());
-	}
-	
-	private static void init() throws Exception { 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 도시 크기
-		M = Integer.parseInt(st.nextToken()); // 치킨집 최대 개수 
-		minDist = Integer.MAX_VALUE; // 도시의 치킨 거리의 최소값
-		selectedChicken = new Coordinate[M]; // 선택된 치킨집 
-		
-		house = new ArrayList<>(); // 집 좌표들 저장
-		chicken = new ArrayList<>(); // 치킨집 좌표들 저장
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int input = Integer.parseInt(st.nextToken());
-				if (input == 1) {
-					house.add(new Coordinate(i, j));
-				} else if (input == 2) {
-					chicken.add(new Coordinate(i, j));
-				}
-			}
-		}
-	}
-	
-	private static void solve() throws Exception {
-		selectChicken(0, 0);
-		sb.append(minDist);
-	}
-	
-	private static void selectChicken(int cnt, int start) {
-		if (cnt == M) {
-			// 선택된 치킨집 기준으로 도시의 최소 치킨 거리 계산
-			sumMinDist();
-			return;
-		}
-		
-		for (int i = start; i < chicken.size(); i++) {
-			selectedChicken[cnt] = chicken.get(i);
-			selectChicken(cnt+1, i+1);
-		}
-	}
-	
-	private static void sumMinDist() {
-		int sumDist = 0;
-		for (Coordinate h : house) {
-			int distance = Integer.MAX_VALUE;
-			for (Coordinate c : selectedChicken) {
-				distance = Math.min(distance, calcDist(h, c));
-			}
-			sumDist += distance;
-		}
-		minDist = Math.min(sumDist, minDist);
-	}
-	
-	
-	private static int calcDist(Coordinate house, Coordinate chicken) {
-		return Math.abs(house.r - chicken.r) + Math.abs(house.c - chicken.c);
-	}
-	
-	static class Coordinate {
-		int r, c;
+    static int N, M, min, map[][], selected[];
+    static Pos[] store, home;
+    static StringBuilder sb = new StringBuilder();
+    static class Pos {
+        int r, c, dist;
 
-		public Coordinate(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
+        public Pos(int r, int c) {
+            this.r = r;
+            this.c = c;
+            dist = Integer.MAX_VALUE;
+        }
+    }
 
-		@Override
-		public String toString() {
-			return "Coordinate [r=" + r + ", c=" + c + "]";
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        selected = new int[M];
+        min = Integer.MAX_VALUE;
+
+        int hcnt = 0, scnt = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) hcnt++;
+                if (map[i][j] == 2) scnt++;
+            }
+        }
+
+        store = new Pos[scnt];
+        home = new Pos[hcnt];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1) home[--hcnt] = new Pos(i, j);
+                if (map[i][j] == 2) store[--scnt] = new Pos(i, j);
+            }
+        }
+
+        combi(0, 0);
+        System.out.println(min);
+    }
+
+    static void combi(int cnt, int start) {
+        if (cnt == M) {
+            sumChickenLoad();
+            return;
+        }
+
+        for (int i = start; i < store.length; i++) {
+            selected[cnt] = i;
+            combi(cnt+1, i+1);
+        }
+    }
+
+    static void sumChickenLoad() {
+        int totalDist = 0;
+        for (int i = 0; i < home.length; i++) {
+            int minDist = Integer.MAX_VALUE;
+            for (int j = 0; j < selected.length; j++) {
+                minDist = Math.min(minDist, calcDistance(home[i], store[selected[j]]));
+            }
+            totalDist += minDist;
+        }
+        min = Math.min(min, totalDist);
+    }
+
+    static int calcDistance(Pos a, Pos b) {
+        return Math.abs(a.r - b.r) + Math.abs(a.c - b.c);
+    }
 }
