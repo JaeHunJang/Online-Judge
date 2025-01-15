18692f2f8cbbd8b7add8f22e725dcd38db3dca1a
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2025-01-15 22:06:48 +0900

    [level 3] Title: 가장 먼 노드, Time: 31.24 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 105 MB, 시간: 91.96 ms
+메모리: 118 MB, 시간: 31.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 01일 21:55:37
+2025년 01월 15일 22:06:45
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,45 +1,39 @@
 import java.util.*;
-
 class Solution {
-    List<Integer> list[];
+    List<Integer>[] edges;
+    
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        edges = new List[n+1];
+        for (int i = 1; i <= n; i++) edges[i] = new ArrayList<>();
         
-        list = new List[n+1];
-        for (int i = 0; i < edge.length; i++) {
-            if (list[edge[i][0]] == null) list[edge[i][0]] = new ArrayList();
-            list[edge[i][0]].add(edge[i][1]);
-            
-            if (list[edge[i][1]] == null) list[edge[i][1]] = new ArrayList();
-            list[edge[i][1]].add(edge[i][0]);
+        for (int[] e : edge) {
+            edges[e[0]].add(e[1]);
+            edges[e[1]].add(e[0]);
         }
         
-        
-        return bfs(n);
+        return bfs();
     }
     
-    public int bfs(int n) {
-        Queue<Integer> q = new ArrayDeque();
+    int bfs() {
+        Queue<Integer> q = new ArrayDeque<>();
         q.offer(1);
-        boolean visited[] = new boolean[n+1];
+        boolean visited[] = new boolean[edges.length];
         visited[1] = true;
         
-        int cnt = 0;
+        int size = 0;
         while(!q.isEmpty()) {
-            int size = q.size();
-            cnt = 0;
+            size = q.size();
             for (int s = 0; s < size; s++) {
                 int now = q.poll();
-                cnt++;
-                
-                if (list[now] == null) continue;
-                for (int next : list[now]) {
+                for (int next : edges[now]) {
                     if (visited[next]) continue;
                     visited[next] = true;
                     q.offer(next);
                 }
             }
         }
-        return cnt;
+        
+        return size;
     }
 }
