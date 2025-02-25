2f8607802ebfa7074fdaf1f1fa5c60dbfcb68c66
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2025-02-25 14:57:04 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,16 +1,14 @@
 class Solution {
     public long solution(int n) {
-        //피보나치 수열 1|1, 2|2, 3|3, 4|5, 5|8, 6|13
-        int[] arr = new int[n];
-        arr[0] = 1;
+        long dp[] = new long[n+3];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
         
-        if(n > 1){
-            arr[1] = 2;
-            for(int i = 2; i < arr.length; i++){
-                arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
-            }
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
         
-        return arr[n-1];
+        return dp[n];
     }
 }
