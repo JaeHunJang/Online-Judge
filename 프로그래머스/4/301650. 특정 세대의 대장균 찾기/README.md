4229912acd5c293557aa70b3384c7b56b3e8c1db
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2026-04-10 13:24:40 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/README.md
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 특정 세대의 대장균 찾기 - 301650 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/301650?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/301650) 
 
 ### 성능 요약
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-합계: 100.0 / 100.0
+합계: true / 100.0
 
 ### 제출 일자
 
-2024년 04월 28일 11:37:19
+2026년 04월 10일 13:24:40
 
 ### 문제 설명
 
--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,7 +1,16 @@
-select id
-from ECOLI_DATA
-where parent_id in (select id
-    from ECOLI_DATA
-    where parent_id in (select id
-        from ECOLI_DATA 
-        where parent_id is null))
+-- 코드를 작성해주세요
+WITH RECURSIVE t as (
+    SELECT id, parent_id, 1 as gen
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+    
+    UNION ALL
+    
+    SELECT c.id, c.parent_id, t.gen + 1
+    FROM ECOLI_DATA c
+    JOIN t ON t.id = c.PARENT_ID
+)
+
+SELECT ID
+FROM t
+WHERE gen = 3
