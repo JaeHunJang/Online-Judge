a6aa25ab8778bc23c5d2e3a837213c7eacdca142
JaeHunJang <46174355+JaeHunJang@users.noreply.github.com>
2026-04-11 22:53:51 +0900

    [level 2] Title: 가장 큰 수, Time: 275.30 ms, Memory: 250 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,21 +1,20 @@
 import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        
-        String[] temp = new String[numbers.length];
-        for(int i = 0; i < temp.length; i++)
-            temp[i] = numbers[i] + "";
-       
-        Arrays.sort(temp, (o1, o2) -> {
-            return (o2 + o1).compareTo(o1 + o2);
+        Integer[] nums = new Integer[numbers.length];
+        for (int i = 0; i < numbers.length; i++) nums[i] = numbers[i];
+        Arrays.sort(nums, (o1, o2) -> {
+            String n1 = o1+""+o2;
+            String n2 = o2+""+o1;
+            return n2.compareTo(n1);
         });
         
-        for(String n : temp)
-            answer += n;
-        
-        if(answer.startsWith("0")) return "0"; //0으로 시작한 경우 제공된 배열이 전부 0
-        
-        return answer;
+        // System.out.println(Arrays.toString(nums));
+        StringBuilder sb = new StringBuilder();
+        for (int n : nums) sb.append(n);
+        if (sb.toString().startsWith("0")) return "0";
+        return sb.toString();
     }
 }
